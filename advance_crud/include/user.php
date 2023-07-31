696c0079ccd5a144f60c65b68683614e3b761d52
Anand Barai <ab20892@gmail.com>
2023-07-31 13:24:51 +0530

    Add files via upload

--- advance_crud/include/user.php
+++ advance_crud/include/user.php
@@ -4,10 +4,31 @@ require_once 'connect.php';
 
 class user extends database
 {
-    protected $tablename="tbl_user";
+    protected $tablename = "tbl_user";
 
     //!Function to add user
+    public function add($data)
+    {
+        if (!empty($data)) {
 
+            //!Initialized as empty arrays
+            $fields = $placeholder = [];
+
+            foreach ($data as $field => $value) {
+                $fields[] = $field;
+                $placeholder[] = ":{field}";
+            }
+        }
+        // $sql = "insert into {$this->tablename} (username,email,mobile) values (:username,:email,:mobile)";
+        $sql = "inseet into {$this->tablename} (" . implode(',', $placeholder) . ") values (" . implode(',', $placeholder) . ")";
+
+        $stmt = $this->conn->prepare($sql);
+        try {
+
+        } catch (PDOException $e) {
+            echo "Error:" . $e->getMessage();
+        }
+    }
 
     //!Function to get rows
 
@@ -17,10 +38,17 @@ class user extends database
 
     //!Function to count number of rows
 
-    
+
     //!Function to upload photo
 
 
+    //!Function to Update
+
+
+    //! Function to Delete
+
+
+    //!Funtion for search
 
 
 
